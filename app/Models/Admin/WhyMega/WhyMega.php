441805d8653647378c3f8755376966805ce2ef59
Anup Shakya <dev2krizmatic.com>
2024-10-03 18:04:28 +0545

    First Commit

---
+++ app/Models/Admin/WhyMega/WhyMega.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Admin\WhyMega;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WhyMega extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_07_27_051330_create_train_your_selves_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainYourSelvesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train_your_selves', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('trip_id');
+            // $table->foreignId('trip_id')->constrained('trips','id')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->longText('train_description')->nullable();
+            $table->string('train_banner_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train_your_selves');
+    }
+}
